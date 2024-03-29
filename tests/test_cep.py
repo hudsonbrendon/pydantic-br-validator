b44924c9cfee01b77ad213371113dfab0ccfcc35
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2024-03-29 17:21:07 -0300

    feat: corrige testes da validação do cep

--- tests/test_cep.py
+++ tests/test_cep.py
@@ -1,130 +1,112 @@
-import re
-
 import pytest
-from faker import Faker
 from pydantic import BaseModel, ValidationError
 
 from pydantic_br_validator import (
-    CNPJ,
-    CNPJDigits,
-    CNPJMask,
-    FieldDigitError,
-    FieldInvalidError,
-    FieldMaskError,
-    FieldTypeError,
+    CEP,
+    CEPDigits,
+    CEPMask,
 )
+from pydantic_br_validator.field_erros import FieldInvalidError, FieldTypeError
 
-TOTAL_CEP = 10
-fake = Faker("pt-BR")
-
-
-def cep_digits():
-    ceps = [re.sub("[^0-9]", "", fake.cep()) for _ in range(TOTAL_CEP)]
-    return ceps
-
-
-def cep_mask():
-    ceps = [fake.cep() for _ in range(TOTAL_CEP)]
-    return ceps
-
-
-def cep_mixed():
-    ceps = [fake.cep() for _ in range(int(TOTAL_CEP / 2))]
-    ceps += [re.sub("[^0-9]", "", fake.cep()) for _ in range(int(TOTAL_CEP / 2))]
-    return ceps
+cep_mock = [
+    ("59151650", "59151-650", "59151650"),
+    ("57602660", "57602-660", "57602660"),
+    ("78735819", "78735-819", "78735819"),
+    ("69315235", "69315-235", "69315235"),
+    ("15056133", "15056-133", "15056133"),
+    ("60761971", "60761-971", "60761971"),
+    ("72760022", "72760-022", "72760022"),
+    ("79020160", "79020-160", "79020160"),
+    ("64040670", "64040-670", "64040670"),
+    ("76965520", "76965-520", "76965520"),
+]
 
 
 @pytest.fixture
-def company():
-    class Company(BaseModel):
-        cnpj: CNPJ
+def address():
+    class Address(BaseModel):
+        cep: CEP
+        cep_mask: CEPMask
+        cep_digits: CEPDigits
 
-    yield Company
-
-
-@pytest.fixture
-def company_masks():
-    class Company(BaseModel):
-        cnpj: CNPJMask
-
-    yield Company
+    yield Address
 
 
-@pytest.fixture
-def company_digits():
-    class Company(BaseModel):
-        cnpj: CNPJDigits
-
-    yield Company
-
-
-@pytest.mark.parametrize("cnpj", cep_mixed())
-def test_must_be_string(company, cnpj):
-    c1 = company(cnpj=cnpj)
-    assert isinstance(c1.cnpj, str)
+@pytest.mark.parametrize("cep, cep_mask, cep_digits", cep_mock)
+def test_must_cep_string(address, cep, cep_mask, cep_digits):
+    address_1 = address(cep=cep, cep_mask=cep_mask, cep_digits=cep_digits)
+    assert isinstance(address_1.cep, str)
+    assert isinstance(address_1.cep_mask, str)
+    assert isinstance(address_1.cep_digits, str)
 
 
-@pytest.mark.parametrize("cnpj", cep_mask())
-def test_mascara_must_be_string(company_masks, cnpj):
-    c1 = company_masks(cnpj=cnpj)
-    assert isinstance(c1.cnpj, str)
+@pytest.mark.parametrize("cep, cep_mask, cep_digits", cep_mock)
+def test_must_accept_only_numbers(address, cep, cep_mask, cep_digits):
+    address_1 = address(cep=cep, cep_mask=cep_mask, cep_digits=cep_digits)
+    assert address_1.cep == cep
+    assert address_1.cep_mask == cep_mask
+    assert address_1.cep_digits == cep_digits
 
 
-@pytest.mark.parametrize("cnpj", cnpj_digits())
-def test_digits_must_be_string(company_digits, cnpj):
-    c1 = company_digits(cnpj=cnpj)
-    assert isinstance(c1.cnpj, str)
-
-
-@pytest.mark.parametrize("cnpj", cnpj_mask())
-def test_must_accept_with_mask(company_masks, cnpj):
-    c1 = company_masks(cnpj=cnpj)
-    assert c1.cnpj == cnpj
-
-
-@pytest.mark.parametrize("cnpj", cnpj_digits())
-def test_must_accept_only_numbers(company_digits, cnpj):
-    c1 = company_digits(cnpj=cnpj)
-    assert c1.cnpj == cnpj
-
-
-@pytest.mark.parametrize("cnpj", cnpj_digits())
-def test_must_fail_when_use_digits_in_mask_class(company_masks, cnpj):
+@pytest.mark.parametrize("cep, cep_mask, cep_digits", cep_mock)
+def test_must_fail_when_use_another_type(address, cep, cep_mask, cep_digits):
     with pytest.raises(ValidationError) as e:
-        company_masks(cnpj=cnpj)
-    assert FieldMaskError.msg_template in str(e.value)
-
-
-@pytest.mark.parametrize("cnpj", cnpj_mask())
-def test_must_fail_when_use_mask_in_digits_class(company_digits, cnpj):
-    with pytest.raises(ValidationError) as e:
-        company_digits(cnpj=cnpj)
-    assert FieldDigitError.msg_template in str(e.value)
-
-
-@pytest.mark.parametrize("cnpj", cnpj_digits())
-def test_must_fail_when_use_another_type(company_digits, cnpj):
-    with pytest.raises(ValidationError) as e:
-        company_digits(cnpj=int(cnpj))
+        address(
+            cep=int(cep),
+            cep_mask=str(cep_mask),
+            cep_digits=int(cep_digits),
+        )
     assert FieldTypeError.msg_template in str(e.value)
 
 
-@pytest.mark.parametrize("cnpj", cnpj_digits())
-def test_must_fail_when_use_invalid_cnpjs(company, cnpj):
+@pytest.mark.parametrize("cep, cep_mask, cep_digits", cep_mock)
+def test_must_fail_when_use_invalid_cep(address, cep, cep_mask, cep_digits):
     with pytest.raises(ValidationError) as e:
-        company(cnpj=cnpj[-1])
+        invalid_cep = cep[:5] + cep[6:]
+        invalid_cep_mask = cep_mask[:5] + cep_mask[6:]
+        invalid_cep_digits = cep_digits[:5] + cep_digits[6:]
+        address(
+            cep=invalid_cep,
+            cep_mask=invalid_cep_mask,
+            cep_digits=invalid_cep_digits,
+        )
     assert FieldInvalidError.msg_template in str(e.value)
 
 
-@pytest.mark.parametrize("cnpj", cnpj_mixed())
-def test_must_fail_when_use_digits_cont_above_cnpjs(company, cnpj):
+@pytest.mark.parametrize("cep, cep_mask, cep_digits", cep_mock)
+def test_must_fail_when_use_digits_count_above_cep(address, cep, cep_mask, cep_digits):
     with pytest.raises(ValidationError) as e:
-        company(cnpj=cnpj * 2)
+        address(cep=cep * 2, cep_mask=cep_mask * 2, cep_digits=cep_digits * 2)
     assert FieldInvalidError.msg_template in str(e.value)
 
 
-@pytest.mark.parametrize("cnpj", cnpj_mixed())
-def test_must_fail_when_use_digits_cont_below_cnpjs(company, cnpj):
+@pytest.mark.parametrize("cep, cep_mask, cep_digits", cep_mock)
+def test_must_fail_when_use_digits_count_below_cep(address, cep, cep_mask, cep_digits):
     with pytest.raises(ValidationError) as e:
-        company(cnpj=cnpj[:5])
+        address(cep=cep[:5], cep_mask=cep_mask[:5], cep_digits=cep_digits[:5])
+    assert FieldInvalidError.msg_template in str(e.value)
+
+
+@pytest.mark.parametrize(
+    "cep, cep_mask, cep_digits",
+    [
+        ("000000000000", "00000-000", "00000000"),
+        ("111111111111", "11111-111", "11111111"),
+        ("222222222222", "22222-222", "22222222"),
+        ("333333333333", "33333-333", "33333333"),
+        ("444444444444", "44444-444", "44444444"),
+        ("555555555555", "55555-555", "55555555"),
+        ("666666666666", "66666-666", "66666666"),
+        ("777777777777", "77777-777", "77777777"),
+        ("888888888888", "88888-888", "88888888"),
+        ("999999999999", "99999-999", "99999999"),
+    ],
+)
+def test_must_fail_when_use_sequecial_digits(address, cep, cep_mask, cep_digits):
+    with pytest.raises(ValidationError) as e:
+        address(
+            cep=cep,
+            cep_mask=cep_mask,
+            cep_digits=cep_digits,
+        )
     assert FieldInvalidError.msg_template in str(e.value)
