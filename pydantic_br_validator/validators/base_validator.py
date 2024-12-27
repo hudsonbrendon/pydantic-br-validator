b85b1bce4aad7e77c65b582c0fc531ca091a21ed
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2024-12-27 02:01:14 -0300

    feat: add uv in project

--- pydantic_br_validator/validators/base_validator.py
+++ pydantic_br_validator/validators/base_validator.py
@@ -3,11 +3,9 @@ from abc import ABC, abstractmethod
 
 class FieldValidator(ABC):
     @abstractmethod
-    def validate(self):
-        ...
+    def validate(self): ...
 
 
 class FieldMaskValidator(FieldValidator):
     @abstractmethod
-    def validate_mask(self):
-        ...
+    def validate_mask(self): ...
